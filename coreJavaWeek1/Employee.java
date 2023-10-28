d275c99aafb84d9f47236837b8f1a88aca2e4d3e
Saurav2k23 <s.bose2k23@gmail.com>
2023-10-28 19:50:40 +0530

    test

---
+++ coreJavaWeek1/Employee.java
@@ -0,0 +1,39 @@
+package coreJavaWeek1;
+
+public class Employee
+{
+	//data
+	int empid;
+	String empname;
+	
+	//function
+	public void empDisplay()
+	{
+		System.out.println("Employee id is: "+empid);
+		System.out.println("Employee name is: "+empname);
+	}
+	
+	
+	public static void main(String args[])
+	{
+		//Object Creation
+		Employee e1=new Employee();
+		e1.empid=101;
+		e1.empname="Seema";
+		e1.empDisplay();
+		System.out.println("***************");
+		Employee e2=new Employee();
+		e2.empid=102;
+		e2.empname="Sarita";
+		e2.empDisplay();
+		System.out.println("***************");
+		Employee e3=new Employee();
+		e3.empid=103;
+		e3.empname="Rakesh";
+		e3.empDisplay();
+		
+		
+	}
+	
+
+}
